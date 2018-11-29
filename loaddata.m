0a072fc3f63be51dd1622a70dde883fd7f954de7
Andruum <andruum@yandex.ru>
2018-11-29 22:05:02 +0200

    Old files deleted

--- loaddata.m
+++
@@ -1,8 +0,0 @@
-function points = loaddata(folder,digit,sample)
-samplenumstr = pad(int2str(sample),4,'left','0');
-file = strcat("stroke_",int2str(digit),"_",samplenumstr,".mat");
-filepath = fullfile(folder,file);
-points = load(filepath);
-points = points.pos;
-end
-
