b6eec8dff9026bfca24d741e615c33435a501ccc
hannah888 <35363577+hannah888@users.noreply.github.com>
2018-12-03 22:32:38 +0200

    Add files via upload
    updated to compute success and fail rates, and display results

--- launch_classify.m
+++ launch_classify.m
@@ -1,11 +1,30 @@
 %% Sample parametes
-addpath("./utils");
-path_folder = "./training_data";
-digit = 8;
-samplen = 90;
+addpath("C:\Users\pauline\Desktop\LUT - Pattern Recognition\Practical assignement - digits_3D\code\utils");
+path_folder = "C:\Users\pauline\Desktop\LUT - Pattern Recognition\Practical assignement - digits_3D\training_data";
 
-%% Load sample
-points = loadsample(path_folder,digit,samplen);
+true_detection = zeros(10, 1) ; % each cell is the number of true detections for the digit (index-1)
+false_detection = zeros (10, 1);  % each cell is the number of false detections for the digit (index-1)
 
-%% Launch test
-C = digit_classify(points)
+for digit = 1:10
+    for s = 71:100
+        
+        %Load sample
+        points = loadsample(path_folder,digit-1,s);
+
+        %Launch test
+        C = digit_classify(points) ;
+        
+        % check results and count errors
+        if (C == digit-1)
+            true_detection(digit) = true_detection(digit) + 1 ;
+        else
+            false_detection(digit) = false_detection(digit) + 1 ;
+        end
+    end
+end
+%%
+% display the results
+
+TD_rate = true_detection./30*100 ;
+FD_rate = false_detection./30*100 ;
+display_results(TD_rate, FD_rate);
