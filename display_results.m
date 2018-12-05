e0307a291d1ad0114e8f2d90256b6bdfab714242
Pauline <pauline.s1502@gmail.com>
2018-12-05 17:23:09 +0200

    small modification

--- display_results.m
+++ display_results.m
@@ -3,8 +3,6 @@ function display_results(TD_rate, FD_rate)
 % parameter TD_rate true detection rate
 % parameter TD_rate false detection rate
 
-close all ;
-figure('Name', 'True detection against false detection');
 labels = {'0','1','2','3','4','5','6','7','8','9'};
 plot(FD_rate,TD_rate,'x');
 text(FD_rate,TD_rate,labels,'VerticalAlignment','bottom','HorizontalAlignment','right');
@@ -14,5 +12,7 @@ plot(100, 0, 'r*'); hold on;
 plot(50, 50, 'm*'); hold on;
 axis([-10 110 -10 110]);
 legend('results', 'best case', 'worst case', 'limit');
+xlabel('False detection rate') ;
+ylabel('True detection rate');
 
 end
